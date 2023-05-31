bd0fc646dde760fc1c3b4f4f690037ae2588fd77
AnaCórdoba <112225465+AnaCordoba@users.noreply.github.com>
2023-05-31 02:34:47 -0500

    Update text-translation.py

--- 06-translate-text/Python/text-translation/text-translation.py
+++ 06-translate-text/Python/text-translation/text-translation.py
@@ -39,31 +39,31 @@ def GetLanguage(text):
     language = 'en'
 
     # Use the Translator detect function
-  # Use the Translator detect function
-  path = '/detect'
-  url = translator_endpoint + path
+    # Use the Translator detect function
+    path = '/detect'
+    url = translator_endpoint + path
 
-  # Build the request
-  params = {
-      'api-version': '3.0'
-  }
+    # Build the request
+    params = {
+        'api-version': '3.0'
+    }
 
-  headers = {
-  'Ocp-Apim-Subscription-Key': cog_key,
-  'Ocp-Apim-Subscription-Region': cog_region,
-  'Content-type': 'application/json'
-  }
+    headers = {
+    'Ocp-Apim-Subscription-Key': cog_key,
+    'Ocp-Apim-Subscription-Region': cog_region,
+    'Content-type': 'application/json'
+    }
 
-  body = [{
-      'text': text
-  }]
+    body = [{
+        'text': text
+    }]
 
-  # Send the request and get response
-  request = requests.post(url, params=params, headers=headers, json=body)
-  response = request.json()
+    # Send the request and get response
+    request = requests.post(url, params=params, headers=headers, json=body)
+    response = request.json()
 
-  # Parse JSON array and get language
-  language = response[0]["language"]
+    # Parse JSON array and get language
+    language = response[0]["language"]
 
     # Return the language
     return language
@@ -72,33 +72,33 @@ def Translate(text, source_language):
     translation = ''
 
     # Use the Translator translate function
-  # Use the Translator translate function
-  path = '/translate'
-  url = translator_endpoint + path
+    # Use the Translator translate function
+    path = '/translate'
+    url = translator_endpoint + path
 
-  # Build the request
-  params = {
-      'api-version': '3.0',
-      'from': source_language,
-      'to': ['en']
-  }
+    # Build the request
+    params = {
+        'api-version': '3.0',
+        'from': source_language,
+        'to': ['en']
+    }
 
-  headers = {
-      'Ocp-Apim-Subscription-Key': cog_key,
-      'Ocp-Apim-Subscription-Region': cog_region,
-      'Content-type': 'application/json'
-  }
+    headers = {
+        'Ocp-Apim-Subscription-Key': cog_key,
+        'Ocp-Apim-Subscription-Region': cog_region,
+        'Content-type': 'application/json'
+    }
 
-  body = [{
-      'text': text
-  }]
+    body = [{
+        'text': text
+    }]
 
-  # Send the request and get response
-  request = requests.post(url, params=params, headers=headers, json=body)
-  response = request.json()
+    # Send the request and get response
+    request = requests.post(url, params=params, headers=headers, json=body)
+    response = request.json()
 
-  # Parse JSON array and get translation
-  translation = response[0]["translations"][0]["text"]
+    # Parse JSON array and get translation
+    translation = response[0]["translations"][0]["text"]
 
 
     # Return the translation
